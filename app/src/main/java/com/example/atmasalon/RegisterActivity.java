39a497c0fcd3c969b91e42d5ba8ef9dc1a369c9e
YOHAN <yohanhadiwijaya@rocketmail.com>
2021-10-10 14:41:52 +0700

    Update Register dan Login XML

---
+++ app/src/main/java/com/example/atmasalon/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.atmasalon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
