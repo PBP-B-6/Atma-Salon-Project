d41b7475e09815a80ac7eafc49d05169faf121d0
YOHAN <yohanhadiwijaya@rocketmail.com>
2021-10-10 14:19:01 +0700

    Clean Project

--- app/src/main/java/com/example/atmasalon/MainActivity.java
+++ app/src/main/java/com/example/atmasalon/MainActivity.java
@@ -16,20 +16,9 @@ public class MainActivity extends AppCompatActivity {
         Thread thread = new Thread() {
             public void run() {
                 try {
-                    sleep(123000);
-                    //yohan
+                    sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                    //Yotam
-                    //Pujayana
-                    //Indonesia
-                    //Yohan indonesia
-                    //sistem
-                    //tes
-                    //2
-                    //apa
-                    //INI AKU UPDATE BANG
-
                 } finally {
                     startActivity(new Intent(MainActivity.this, LoginActivity.class));
                     finish();
@@ -37,17 +26,5 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         thread.start();
-//        try {
-//            Thread.sleep(6000);
-//            Intent move = new Intent(this, LoginActivity.class);
-//            startActivity(move);
-//            finish();
-//
-//
-//        }
-//        catch(Exception e)
-//        {
-//            System.out.println(e.getMessage());
-//        }
     }
 }
